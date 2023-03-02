87b5b88dc7be821b3460e63be9300260a88e1149
Ryan Li <ryan.li@mirumagency.com>
2023-03-02 08:38:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        BACK TO THE BEGINNING
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+div {
+    font-family: font-2;
+    font-size: 2.5rem;
+    color: var(--octonary-);
+}
+</style>
